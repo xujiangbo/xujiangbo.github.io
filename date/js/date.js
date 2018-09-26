713d140b5f616804b911ef10c816797a07a417dd
jiangboxu <jiangbo.xu@yunshipei.com>
2018-09-26 16:34:49 +0800

    修改首页

--- date/js/date.js
+++ date/js/date.js
@@ -1,5 +1,14 @@
 
 
+
+
+
+
+
+
+
+
+
 setInterval(function(){
   var date = new Date();
   var time = date.getHours();
@@ -8,15 +17,23 @@ setInterval(function(){
   console.log(time+'时'+min+'分'+seconds+'秒');
   var timebox = document.getElementById('time');
   $('.time').css({
-      'transform-origin':'100% 100%',
+      'transform-origin':'50% 100%',
       'transform':'rotate('+ 360/12*time +'deg)'
   })
   $('.min').css({
-      'transform-origin':'100% 100%',
+      'transform-origin':'50% 100%',
       'transform':'rotate('+ 360/60*min +'deg)'
   })
   $('.sec').css({
-      'transform-origin':'100% 100%',
+      'transform-origin':'50% 100%',
       'transform':'rotate('+ 360/60*seconds +'deg)'
   })
+
+  $("#house>h1").text(function(){
+    return new Date().getFullYear()+'年'+new Date().getMonth()+'月'+new Date().getDate()+'日'
+  })
+    $("#house>h2").text(function(){
+    return new Date().getHours()+':'+new Date().getMinutes()+':'+new Date().getSeconds();
+  })
+
 },1000)
